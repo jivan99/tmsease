2f1dc0f310b3fb99dc0385cbd488b936ad623fd0
jivan99 <jivan.katwal99@gmail.com>
2024-12-30 23:01:08 +0545

    feat: add keyboard shortcut to signout

--- src/content/main.js
+++ src/content/main.js
@@ -1,5 +1,13 @@
 import "@/assets/style.css";
 import App from "./App.vue";
+import { onMessage } from "webext-bridge/content-script";
+
+onMessage("SIGNOUT", async () => {
+  const logoutBtn = document.querySelector("div.ubadge__dropdown a:last-child");
+  if (logoutBtn) {
+    logoutBtn.click();
+  }
+});
 
 const app = createApp(App);
 const root = document.createElement("div");
