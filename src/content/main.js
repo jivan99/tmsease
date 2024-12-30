780f71f225bc0beab64748c689b1e11411e6bfd0
jivan99 <jivan.katwal99@gmail.com>
2024-12-30 18:59:03 +0545

    feat(content): setup vue.js

--- src/content/main.js
+++ src/content/main.js
@@ -1,1 +1,8 @@
-console.log("hello from content script.");
+import App from "./App.vue";
+
+const app = createApp(App);
+const root = document.createElement("div");
+root.id = "tms-ease";
+document.body.appendChild(root);
+
+app.mount(root);
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,25 @@
 import { defineConfig } from "vite";
+import Vue from "@vitejs/plugin-vue";
 import { crx } from "@crxjs/vite-plugin";
+import { fileURLToPath, URL } from "node:url";
+import AutoImport from "unplugin-auto-import/vite";
 
 import manifest from "./manifest.json" with { type: "json" };
 
 export default defineConfig({
-  plugins: [crx({ manifest })]
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
+  },
+  plugins: [
+    Vue(),
+    AutoImport({
+      imports: ["vue"]
+    }),
+    crx({ manifest })
+  ],
+  optimizeDeps: {
+    include: ["vue"]
+  }
 });
