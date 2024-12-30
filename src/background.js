4409ced0bb971bb8e269ecef078a9bd4bcf50f02
jivan99 <jivan.katwal99@gmail.com>
2024-12-30 23:49:46 +0545

    feat(content): mount UI only in login page

--- src/background.js
+++ src/background.js
@@ -4,6 +4,15 @@ browser.runtime.onInstalled.addListener(() => {
   console.log("Howdy! from background script.");
 });
 
+browser.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  if (changeInfo.status === "complete" && tab?.url?.includes("login")) {
+    await sendMessage("MOUNT_APP", {}, `content-script@${tabId}`);
+    return;
+  }
+
+  await sendMessage("UNMOUNT_APP", {}, `content-script@${tabId}`);
+});
+
 browser.commands.onCommand.addListener(async (command) => {
   if (command === "SIGNOUT") {
     const tabs = await browser.tabs.query({
